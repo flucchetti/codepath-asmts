2f281d710bfdc13d171881be01d3081523deed68
Francesca <francesca@Francescas-MacBook-Pro.local>
2021-12-04 14:48:47 -0500

    added trailer

--- week1-flix/week1-flix/MovieDetailsViewController.swift
+++ week1-flix/week1-flix/MovieDetailsViewController.swift
@@ -11,6 +11,7 @@ import AlamofireImage
 class MovieDetailsViewController: UIViewController {
 
     var movie : [String:Any]!
+    var videoResponse : [[String:Any]]!
     @IBOutlet weak var backdropView: UIImageView!
     @IBOutlet weak var posterView: UIImageView!{ didSet { posterView.isUserInteractionEnabled = true }}
     @IBOutlet weak var titleLabel: UILabel!
@@ -19,6 +20,24 @@ class MovieDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let movieID = movie["id"] as! Int
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/" + String(movieID) + "/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+    
+                 self.videoResponse = dataDictionary["results"] as? [[String:Any]]
+             }
+        }
+        task.resume()
+        
 
         titleLabel.text = movie["title"] as? String
         titleLabel.sizeToFit()
@@ -46,31 +65,10 @@ class MovieDetailsViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let movieID = movie["id"] as! Int
-        var key : String!
-        
-        let url = URL(string: "https://api.themoviedb.org/3/movie/" + String(movieID) + "/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-             // This will run when the network request returns
-             if let error = error {
-                    print(error.localizedDescription)
-             } else if let data = data {
-                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-    
-                 let results = dataDictionary["results"] as! [[String:Any]]
-                 let video = results[0]
-                 key = video["key"] as! String
-             }
-        }
-        
+        let video = videoResponse[0]
+        let key = video["key"] as! String
         let trailerViewController = segue.destination as! TrailerViewController
-//        need to change key here:
-        trailerViewController.movieURL = URL(string :"https://www.youtube.com/watch?v=0")
-        
-        task.resume()
-        
+        trailerViewController.movieURL = URL(string :"https://www.youtube.com/watch?v=" + key)
         
     }
 }
